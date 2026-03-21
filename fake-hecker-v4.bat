fb79a4e4a22a4a41eaee298c39fea73fce11ac29
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-21 11:37:41 -0300

    Create fake-hecker-v4.bat

---
+++ fake-hecker-v4.bat
@@ -0,0 +1,564 @@
+color 2
+echo -------------Terminal-------------
+timeout /t 2 >nul
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo Starting hacks...
+cd \Windows\Panther\
+dir
+cd \Windows\System32\
+dir
+cd \Windows\WinSxS\
+dir
+echo %random%
+echo -------------Finished-------------
